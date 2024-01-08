3a44fd53394dff4c2479abbc504c531e32e5430b
sa30h <satishrjk07@gmail.com>
2024-01-08 22:59:46 +0530

    add: bootstrap,navbar

--- maloneyWebApp/maloneyWebApp/settings.py
+++ maloneyWebApp/maloneyWebApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'maloneyWebApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,8 +133,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR,'static')
+]
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
+
+# AUTH_USER_MODEL = ('authentication.User')
+
+#email sending variable
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'satishrjk07@gmail.com'
+# EMAIL_HOST_PASSWORD = 'lrumqmolxpeoagip'
+# CRONJOBS = [
+#     ('* * 13 * *', 'hd.cron.Update_cron')
+# ]
+
+
+CORS_ORIGIN_ALLOW_ALL = True  
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
